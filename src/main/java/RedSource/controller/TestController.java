c01abb20b59b992c4eb40ab1f7419f7c744367b9
IrishLince <lince2221329@mkt.ceu.edu.ph>
2025-09-14 04:58:20 +0800

    Fix TestController compilation errors - add missing imports and dependencies

--- src/main/java/RedSource/controller/TestController.java
+++ src/main/java/RedSource/controller/TestController.java
@@ -1,15 +1,18 @@
 package RedSource.controller;
 
 import RedSource.entities.Token;
+import RedSource.repositories.TokenRepository;
 import RedSource.services.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/test")
@@ -20,6 +23,9 @@ public class TestController {
 
     @Autowired
     private TokenService tokenService;
+    
+    @Autowired
+    private TokenRepository tokenRepository;
 
     @GetMapping("/db")
     public ResponseEntity<?> testDatabase() {
